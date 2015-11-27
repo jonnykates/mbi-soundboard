05f5ab8d616575d0b7aa661f95217c5da168f1f7
Jonny Kates <hi@jonnykates.com>
2015-11-27 20:16:29 +0000

    Angularised and Jqueried

---
+++ js/controllers/SoundController.js
@@ -0,0 +1,13 @@
+/*global app:true */
+app.controller('SoundController', ['$scope', function($scope) {
+
+$scope.sounds = [
+
+  {
+    title: 'test',
+    coverImage: 'img/taylor-1.png'
+  }
+
+];
+
+}]);
